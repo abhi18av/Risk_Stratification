3b779854b90efc0f2e91f6a2f06ab290dc508c72
Abhinav Sharma <abhi18av@gmail.com>
2020-02-09 18:57:49 +0530

    identified the missing values and continuing with EDA

--- src/_scratch.py
+++ src/_scratch.py
@@ -34,6 +34,7 @@ print_ln()
 diabetic_patient_data.head(5)
 
 
+# Identifying the numerical and categorical features
 def type_features(data):
     categorical_features = data.select_dtypes(include=["object"]).columns
     numerical_features = data.select_dtypes(exclude=["object"]).columns
@@ -48,14 +49,29 @@ diabetic_patient_data_cat_features, diabetic_patient_data_num_features = type_fe
 
 # TODO
 """
-Remove redundant variables, duplicate rows/columns
+Remove redundant variables
 """
+# NOTE replace the `?` as `nan`
+# https://stackoverflow.com/questions/52643775/how-to-replace-specific-character-in-pandas-column-with-null
+
+# diabetic_patient_data['weight'] = diabetic_patient_data['weight'].replace('?', np.nan)
+
+diabetic_patient_data = diabetic_patient_data.replace('?', np.nan)
+
+"""
+Remove duplicated rows/columns
+"""
+
+duplicated_data = diabetic_patient_data[diabetic_patient_data.duplicated()]  # seems like there is no duplicated data
 
 # TODO
 """
 Check for missing values and treat them accordingly.
 
 """
+
+# Analyse the missing values
+diabetic_patient_data.isnull().sum()  # seems like there is no missing data
 
 # TODO
 """
@@ -73,4 +89,53 @@ Change the variable 'readmitted' to binary type by clubbing the values ">30" and
 """
 Create the derived metric 'comorbidity', according to the following scheme -
 
+"""
+
+# ==================
+# Data Exploration
+# ==================
+
+
+# TODO
+"""
+Perform basic data exploration for some categorical attributes
+"""
+
+# TODO
+"""
+Perform basic data exploration for some numerical attributes
+"""
+
+# ==================
+# Model Building
+# ==================
+
+
+# TODO
+"""
+Divide your data into training and testing dataset
+"""
+# TODO
+"""
+Train and compare the performance of at least two machine learning algorithms and decide which one to use for predicting risk of readmission for the patient.
+"""
+# TODO
+"""
+Important feature for each model is calculated.
+"""
+# TODO
+"""
+Use trained model to stratify your population into 3 risk buckets:
+"""
+# TODO
+"""
+High risk (Probability of readmission >0.7)
+"""
+# TODO
+"""
+Medium risk (0.3 < Probability of readmission < 0.7)
+"""
+# TODO
+"""
+Low risk (Probability of readmission < 0.3)
 """
